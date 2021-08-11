e8b7d939cadf360d3ac003705bd98763477a988b
github@matthewlloyd.net <github@matthewlloyd.net>
2021-08-11 13:03:46 -0400

    Add support for M300 (play sound)

---
+++ src/marlin_stubs/M300.hpp
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021 Matthew Lloyd <github@matthewlloyd.net>
+ * All rights reserved.
+ *
+ * This file is part of Llama Mini.
+ *
+ * Llama Mini is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Llama Mini is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Llama Mini. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+namespace PrusaGcodeSuite {
+void M300(); // play sound
+}
--- src/marlin_stubs/PrusaGcodeSuite.hpp
+++ src/marlin_stubs/PrusaGcodeSuite.hpp
@@ -3,4 +3,5 @@
 namespace PrusaGcodeSuite {
 void G26();  /// first layer calibration
 void G162(); /// handler-specific configuration
+void M300(); /// M300 play sound.
 }
